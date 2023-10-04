3a69e8102c4e7433f263baeb1c2b36295d590449
vishal253 <vishalsyn123@gmail.com>
2023-10-04 21:53:32 +0530

    As a Automation Tester I have completed the Hybrid testNG Framework Project which includes page object model and page factory design patterns. I have also automated the few test cases of Login, Register and search functionality to initiate the automation framework.

---
+++ src/test/java/com/tutorialsninja/qa/testcases/LoginTest.java
@@ -0,0 +1,93 @@
+package com.tutorialsninja.qa.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.tutorialsninja.qa.base.Base;
+import com.tutorialsninja.qa.pages.AccountPage;
+import com.tutorialsninja.qa.pages.HomePage;
+import com.tutorialsninja.qa.pages.LoginPage;
+import com.tutorialsninja.qa.utils.Utilities;
+
+public class LoginTest extends Base{
+	
+	LoginPage loginPage;
+	
+	public LoginTest() {
+		super();
+	}
+	
+	 WebDriver driver ;
+	
+	@BeforeMethod
+	public void setup() {
+		
+		driver = initializeBrowserAndOpenApplicationURL(prop.getProperty("browserName"));
+		HomePage homePage = new HomePage(driver);
+		loginPage = homePage.naviageToLoginPage();
+		
+	
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\vishal\\Downloads\\TutorialsNinjaHybridFrameworkRepo-master\\TutorialsNinjaHybridFrameworkRepo-master\\driver_\\chromedriver.exe");
+		// driver =new ChromeDriver();
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		
+		driver.quit();
+		
+	}
+
+	@Test(priority=1,dataProvider="validCredentialsSupplier")
+	public void verifyLoginWithValidCredentials(String email,String password) {
+	
+		AccountPage accountPage = loginPage.login(email, password);
+		Assert.assertTrue(accountPage.getDisplayStatusOfEditYourAccountInformationOption(),"Edit Your Account Information option is not displayed");
+	
+	}
+	
+	@DataProvider(name="validCredentialsSupplier")
+	public Object[][] supplyTestData() {
+		
+		Object[][] data = Utilities.getTestDataFromExcel("Login");
+		return data;
+	}
+	
+	@Test(priority=2)
+	public void verifyLoginWithInvalidCredentials() {
+		
+		loginPage.login(Utilities.generateEmailWithTimeStamp(),dataProp.getProperty("invalidPassword"));	
+		Assert.assertTrue(loginPage.retrieveEmailPasswordNotMatchingWarningMessageText().contains(dataProp.getProperty("emailPasswordNoMatchWarning")),"Expected Warning message is not displayed");
+		
+	}
+	
+	@Test(priority=3)
+	public void verifyLoginWithInvalidEmailAndValidPassword() {
+	
+		loginPage.login(Utilities.generateEmailWithTimeStamp(),prop.getProperty("validPassword"));
+		Assert.assertTrue(loginPage.retrieveEmailPasswordNotMatchingWarningMessageText().contains(dataProp.getProperty("emailPasswordNoMatchWarning")),"Expected Warning message is not displayed");
+	
+	}
+	
+	@Test(priority=4)
+	public void verifyLoginWithValidEmailAndInvalidPassword() {
+		
+		loginPage.login(prop.getProperty("validEmail"),dataProp.getProperty("invalidPassword"));		
+		Assert.assertTrue(loginPage.retrieveEmailPasswordNotMatchingWarningMessageText().contains(dataProp.getProperty("emailPasswordNoMatchWarning")),"Expected Warning message is not displayed");
+
+	}
+	
+	@Test(priority=5)
+	public void verifyLoginWithoutProvidingCredentials() {
+		
+		loginPage.clickOnLoginButton();
+		Assert.assertTrue(loginPage.retrieveEmailPasswordNotMatchingWarningMessageText().contains(dataProp.getProperty("emailPasswordNoMatchWarning")),"Expected Warning message is not displayed");
+	
+	}
+	
+}
